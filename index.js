f2101dcfeb6bec1f931493ade0f1ac7ebe622c6c
Andreas Wålm <andreas@walm.net>
2018-02-09 07:04:37 +0100

    add notification signup

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('static'));
 
+const { MONGO_URL } = process.env
+const mongo = mongoose.createConnection(MONGO_URL)
+mongoose.Promise = global.Promise
+
+const HelloNotify = mongo.model('HelloNotify', mongoose.Schema({ email: String, c_at: Date }))
+
 app.get('/status', (req, res) => {
   res.send("I'm running");
+});
+
+app.post('/notify', (req, res) => {
+  let email = (req.body.email || '').trim()
+  if (email == '') return res.json({ message: 'Något saknades', code: 400 })
+
+  console.log(`Notifying ${email}`)
+  const rec = new HelloNotify({ email: email, c_at: new Date()})
+  rec.save().then((doc) => {
+    res.json({ success: true, code: 0 })
+  })
 });
 
 app.post('/subscribe', (req, res) => {
--- static/js/main.js
+++ static/js/main.js
@@ -5,7 +5,9 @@ new Vue({
 
   data: {
     error: false,
-    loading: false
+    loading: false,
+    typing: false,
+    email: ""
   },
 
   created: function() {
@@ -13,6 +15,21 @@ new Vue({
   },
 
   methods: {
+
+    validEmail: function(s) {
+      return this.email.trim().match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{1,20}$/i);
+    },
+
+    notify: function() {
+      if (!this.validEmail()) {
+        return alert("E-postadressen ser inte riktigt rätt ut");
+      }
+      var self = this;
+      jQuery.post("/notify", { email: self.email.trim() }, function(){
+        self.email = "";
+        alert("Super, vi meddelar när allt är uppe. Tack!");
+      });
+    }
 
   }
 });
